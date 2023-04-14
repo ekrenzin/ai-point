d12863284b73b8f3b5d0987bf82e32a8196d3cf8
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-04-14 13:20:41 -0700

    add open ai

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import Cheerio from 'cheerio';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+
+const app = express()
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const basePrompt = "Analyze this HTML. Respond with a title, and summary of the article. Include links to any images or videos in an array. Format in a JSON object. {title, summary, images, videos}. The summary should be about the content of the article, not the content itself. Explain the purpose of the page, but do not summarize it."
+
+app.use(express.json()); // Make sure this line is present to parse the incoming JSON request body
+
+app.post('/', async function (req, res) {
+
+  try {
+    console.log(req.body)
+    //get the url from the body
+    const url = req.body.url
+    //get the html from the url
+    const htmlRes = await fetch(url)
+    const html = await htmlRes.text()
+    const body = parseOutHtml(html)
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: basePrompt + body,
+      temperature: 0.6,
+      max_tokens: 500,
+    });
+    const text = completion.data.choices[0].text
+    console.log(text)
+    res.status(200).json({ result: text });
+  } catch (error) {
+    console.error(error);
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+})
+
+function parseOutHtml(html) {
+
+  // Load the HTML string into cheerio
+  const $ = Cheerio.load(html);
+  const bodyContent = $('body').html();
+  return bodyContent
+}
+
+app.listen(8080)
+
+export {
+  app
+}
--- test/api.test.js
+++ test/api.test.js
@@ -1,15 +1,19 @@
-const assert = require('assert')
-const request = require('supertest')
-const app = require('../index.js')
+import request from 'supertest';
+import {app} from '../src/index.js';
 
+const url = "https://markdown-cloud.devon.workers.dev/api/documents/b1ff2dc8-edef-471b-a987-9f671327d943"
 //test the localhost server at 3000 port
 describe('API', async function() {
     this.timeout(50000); // Adjust the number to a suitable value (in milliseconds)
     it('should return a completion', async function() {
         await request(app)
-        .get('/')
+        .post('/')
+        .send({
+          url,
+        })
         .expect(200) // check that the status code is 200 OK
-        .expect('Content-Type', /html/); // check that the response is HTML
+        .expect('Content-Type', /json/); // check that the response is HTML
     });
 });
 
+//test the aws server at 300
